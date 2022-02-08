81dee43144e9cac9ab98cda77108b1a13f43ad73
Sikhor <99123525+Sikhor@users.noreply.github.com>
2022-02-08 11:01:11 +0100

    New Batch template  to play as client

---
+++ playclient-template.bat
@@ -0,0 +1,8 @@
+REM IMPORTANT STEPS TO USE THIS
+REM 1.) Rename this file to playclient.bat - otherwise it will be overwritten bei source control
+REM 2.) Replace <UE4Editor> with your full path to UE4Editor !!!Watch out for blanks.. then path must be in ""
+REM 3.) Replace <GAMEPROJECT> with your Game Project path !!!Use full path to the uproject file 
+REM 4.) Replace <IPADDRESS> with the your IP Adress of your game server (Can be Listen Server too)
+REM At the end it should look like this example
+REM "c:\program files\Epic Games\UE4.27\engine\binaries\win64\ue4editor.exe" "c:\user\admin\documents\unrealprojects\MMOTutorials\MMOTutorials.uproject" 44.88.0.3 -game
+"<UE4EDITOR>" "<GAMEPROJECT>" <IPADDRESS> -game
